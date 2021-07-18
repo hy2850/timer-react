b8c6e0b339927ea9c7d769673962763d1a32c4ee
hy2850 <39735858+hy2850@users.noreply.github.com>
2021-07-18 21:39:32 +0900

    [Create] Toggle second timer
    - on App.js, using state 'toggleSecond', implemented second timer component toggling

--- src/App.js
+++ src/App.js
@@ -5,19 +5,28 @@ import React, {useState, useEffect} from 'react';
 import SettingsModal from './SettingsModal';
 
 function App(props) {
+  const [toggleSecond, setToggleSecond] = useState(false);
+
   // add header
   // add timer
-
   return (
     <div>
+      <div class="header">
+        <span class="title">Pomodoro Timer Plus+</span>
+        <button class="toggle-long" onClick = {()=>setToggleSecond(!toggleSecond)}>Long-Timer</button>
+      </div>
+
       <Timer 
         timer_type = "SHORT" 
         timerTime = {3}> 
       </Timer>
-      <Timer 
-        timer_type = "LONG" 
-        timerTime = {5}> 
-      </Timer>
+      {toggleSecond ?
+        <Timer 
+          timer_type = "LONG" 
+          timerTime = {5}> 
+        </Timer>
+        : null
+      }
     </div>
   );
 }
